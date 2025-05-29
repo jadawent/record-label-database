47952455726e29352cb1d6fa27ed3d7693398969
jadawent <jadawent@gmail.com>
2025-05-29 16:20:09 -0500

    Added all files from project to github

---
+++ src/main/java/matthew_klein/musicdbphase4/Band.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package matthew_klein.musicdbphase4;
+import java.time.LocalDate;  
+
+
+/**
+ *
+ * @author mklei
+ */
+public class Band {
+    public String band_name;
+    public Integer member_cound;
+    public LocalDate date;
+    public Boolean is_active;
+    public Boolean is_hiring;
+}
---
+++ src/main/java/matthew_klein/musicdbphase4/Song.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package matthew_klein.musicdbphase4;
+
+/**
+ *
+ * @author jadaw
+ */
+public class Song {
+    public String song_name;
+    public String band_name;
+    public String album_name;
+    public int play_count;
+}
